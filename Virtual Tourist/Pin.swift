1948c41d55caae59846dbe053b51e331351e67ef
josephdhooper <jdhooper@gmail.com>
2016-04-16 14:22:11 -0400

    Add source information and icon.

--- Virtual Tourist/Pin.swift
+++ Virtual Tourist/Pin.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Joseph Hooper on 4/14/16.
 //  Copyright © 2016 josephdhooper. All rights reserved.
-
+//  Code from https://github.com/jarrodparkes/virtual-tourist.git and other Udacity-focused repositories was repurposed for this project.
 
 import CoreData
 import MapKit
